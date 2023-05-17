ad3bc02bfe4c82b4b908fcf3c998b1cd645e0f77
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-05-16 21:17:47 -0400

    Built Brows URL history

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -31,7 +31,7 @@ const Demo = () => {
 
             setArticle(newArticle)
             setAllArticles(updatedAllArticles)
-            
+
             localStorage.setItem('articles', JSON.stringify(updatedAllArticles))
         } 
     }
@@ -56,7 +56,26 @@ const Demo = () => {
                   <button type="submit" className="submit_btn">↩</button>
               </form>
               {/* Browse URL History */}
-
+            <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+                  {allArticles.map((item, index) => (
+                    <div
+                        key={`link-${index}`}
+                        onClick={() => setArticle(item)}
+                        className="link_card"
+                      >
+                        <div className="copy_btn">
+                                <img 
+                                    src={copy}
+                                    alt="copy_icon"
+                                        className="w-[40%] h-[40%] object-contain"
+                            />
+                        </div>
+                          <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                              {item.url}
+                          </p>
+                    </div>
+                  ))}
+            </div>
           </div>
           {/* Display Results */}
     </section>
